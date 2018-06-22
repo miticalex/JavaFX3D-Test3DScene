e0ee519240576f5d896054557b6ab2db31ed8206
Alexander Mitic <miticalexander@yahoo.com>
2018-06-22 21:16:32 +0200

    Project finished
    - Added a method that shows if 3DScene is supported on your machine

--- Test3DScene/src/test3dscene/Test3DScene.java
+++ Test3DScene/src/test3dscene/Test3DScene.java
@@ -6,8 +6,11 @@
 package test3dscene;
 
 import javafx.application.Application;
+import javafx.application.ConditionalFeature;
+import javafx.application.Platform;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 /**
@@ -15,11 +18,15 @@ import javafx.stage.Stage;
  * @author AM
  */
 public class Test3DScene extends Application {
+    private static final double WINDOW_WIDTH = 300;
+    private static final double WINDOW_HEIGHT = 50;
+    
     Group root = new Group();
     
     @Override
     public void start(Stage primaryStage) {
-        Scene scene = new Scene(root, 300, 250);
+        Scene scene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT);
+        showIsSupported();
         
         primaryStage.setTitle("Test 3D Scene!");
         primaryStage.setScene(scene);
@@ -33,4 +40,17 @@ public class Test3DScene extends Application {
         launch(args);
     }
     
+    private void showIsSupported(){
+        String text = Platform.isSupported(ConditionalFeature.SCENE3D) ? 
+                "3D Scene is supported on your machine." :
+                "3D Scene is not supported on your machine.";
+                
+        Text textField = new Text(text);
+        textField.setTranslateX(WINDOW_WIDTH/2
+                - textField.getBoundsInParent().getWidth()/2);
+        textField.setTranslateY(WINDOW_HEIGHT/2
+                + textField.getBoundsInParent().getHeight()/3);
+                
+        root.getChildren().addAll(textField);
+    }
 }
